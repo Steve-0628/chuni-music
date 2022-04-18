8af54e1daf07d8d0659c2c07755f92b1a3b9a83f
Steve-0628 <github@hmpf.club>
2022-04-18 22:10:09 +0900

    working except world's end

--- viewer/pages/index.vue
+++ viewer/pages/index.vue
@@ -1,32 +1,54 @@
-<script setup lang="ts">
+<script setup lang="ts" ssr="false">
 
-const chunirec_region = "jp2"
-const chunirec_token = "YOUR_CHUNIREC_TOKEN"
-const chunirec_url = new URL("https://api.chunirec.net/2.0/music/showall.json")
-chunirec_url.searchParams.append("region", chunirec_region)
-chunirec_url.searchParams.append("token", chunirec_token)
-const chunirec_data = await fetch(chunirec_url.href).then(chunirec => chunirec.json())
-
-const sega_data = await JSON.parse((await useFetch("/api/all")).data.value)
-const sega_parsed = {}
-sega_data.forEach(item => {
-    sega_parsed[item["title"]] = item["image"]
-});
-
+let chunirec_token = ""
+if(!chunirec_token){
+    // alert("oi")
+}
 const musics = {}
-chunirec_data.forEach(music => {
-    let {genre} = music["meta"]
-    // categories_set.add(gener)
-    if(sega_parsed[music["meta"]["title"]]){
-        music["image"] = sega_parsed[music["meta"]["title"]]
+
+let category = ref("")
+let categories = ref([])
+
+onMounted(async () => {
+    chunirec_token = localStorage.chunirec_token
+    if(!chunirec_token){
+        alert("chunirec_tokenがありません。chunirec_tokenを設定してください。")
+        chunirec_token = prompt("ここにchunirecで取得したトークンをペースト")
+        localStorage.chunirec_token = chunirec_token
     }
-    musics[genre] ||= []
-    musics[genre].push(music)
+
+    const chunirec_region = "jp2"
+    const chunirec_url = new URL("https://api.chunirec.net/2.0/music/showall.json")
+    chunirec_url.searchParams.append("region", chunirec_region)
+    chunirec_url.searchParams.append("token", chunirec_token)
+    const chunirec_data = await fetch(chunirec_url.href).then(chunirec => chunirec.json())
+
+    const sega = (await useFetch("/api/all")).data.value
+    // console.log(sega)
+    const sega_data = await JSON.parse(sega)
+    const sega_parsed = {}
+    sega_data.forEach(item => {
+        sega_parsed[item["title"]] = item["image"]
+    });
+
+
+    chunirec_data.forEach(music => {
+        let {genre} = music["meta"]
+        // categories_set.add(gener)
+        if(sega_parsed[music["meta"]["title"]]){
+            music["image"] = sega_parsed[music["meta"]["title"]]
+        }
+        musics[genre] ||= []
+        musics[genre].push(music)
+    })
+    categories.value = Object.keys(musics)//Array.from(categories_set)
+
+    category.value = categories[0]
 })
-const categories: Array<string> = Object.keys(musics)//Array.from(categories_set)
 
-const category = ref(categories[0])
 
+</script>
+<script>
 </script>
 
 <style>
@@ -85,22 +107,131 @@ body {
 }
 .music_top {
     height: 40vmin;
-    background-color: purple;
+    background-color: beige;
 }
 .music_top_container {
     display:flex;
     flex-direction: column;
     justify-content: space-between;
+    height:100%;
 }
 .music_top_img_container {
     display:flex;
 }
 .music_top_img_container > img {
     width: 35%;
-    height: 35%;
+    /* height: 35%; */
     margin-left: 5%;
     margin-top: 5%;
     margin-left: 5%;
+}
+.music_top_lowlevel_container {
+    margin:5%;
+    margin-bottom:0;
+    flex-grow: 1;
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.basic_container {
+    height:6vmin;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+.basic {
+    border: 0.5vmin white solid;
+    width:5.5vmin;
+    height:5.5vmin;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: green;
+    color:white;
+    font-weight: bold;
+}
+.advanced_container {
+    height:6vmin;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+.advanced {
+    border: 0.5vmin white solid;
+    width:5.5vmin;
+    height:5.5vmin;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: orange;
+    color:white;
+    font-weight: bold;
+}
+.music_highlevel_container{
+    margin: 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    flex-grow: 1;
+}
+.expert {
+    /* flex-grow: 1; */
+    height:8vmin;
+    background-color: white;
+    display:flex;
+}
+.expert_container {
+    background-color: red;
+    margin:0.5vmin;
+    height:7vmin;
+    width:7vmin;
+    color:white;
+    display:flex;
+    text-align: center;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    font-weight:bold;
+}
+.expert_detail {
+    flex-grow: 1;
+    margin:0.5vmin;
+    display:flex;
+    flex-direction:column;
+    justify-content: space-around;
+}
+.master {
+    /* flex-grow: 1; */
+    height:8vmin;
+    background-color: white;
+    display:flex;
+}
+.master_container {
+    background-color: purple;
+    margin:0.5vmin;
+    height:7vmin;
+    width:7vmin;
+    color:white;
+    display:flex;
+    text-align: center;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    font-weight:bold;
+}
+.master_detail {
+    flex-grow: 1;
+    margin:0.5vmin;
+    display:flex;
+    flex-direction:column;
+    justify-content: space-around;
+}
+.music_high_level {
+    font-size:90%;
+    font-weight: lighter;
+}
+.music_high_level_val {
+    font-size:120%;
 }
 
 .music_bottom {
@@ -110,12 +241,15 @@ body {
     flex-direction: column;
 }
 .music_title {
-    font-size: 1.2rem;
+    font-size: 1.5rem;
 }
 .divider {
     width: 80%;
     height:2px;
     background-color: darkgray;
+}
+.music_artist {
+    overflow:scroll
 }
 .music_bottom_container {
     width: 100%;
@@ -145,17 +279,53 @@ body {
                             <div class="music_top_img_container">
                                 <img v-bind:src="'https://new.chunithm-net.com/chuni-mobile/html/mobile/img/'+item['image']" >
                                 <div class="music_top_lowlevel_container">
-                                    <div>basic:{{item["data"]["BAS"]["level"]}}</div>
-                                    <div>advanced:{{item["data"]["ADV"]["level"]}}</div>
+                                    <div class="basic_container">
+                                        <div class="basic">{{item["data"]["BAS"]["level"]}}</div>
+                                        <div>最大コンボ数</div>
+                                        <div>{{item["data"]["BAS"]["maxcombo"]}}</div>
+                                    </div>
+                                    <div class="advanced_container">
+                                        <div class="advanced">{{item["data"]["ADV"]["level"]}}</div>
+                                        <div>最大コンボ数</div>
+                                        <div>{{item["data"]["ADV"]["maxcombo"]}}</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div>expert:{{item["data"]["EXP"]["level"]}}</div>
-                            <div>master:{{item["data"]["MAS"]["level"]}}</div>
+                            <div class="music_highlevel_container">
+                                <div class="expert">
+                                    <div class="expert_container">
+                                        <div class="music_high_level">
+                                            LEVEL
+                                        </div>
+                                        <div class="music_high_level_val">
+                                            {{item["data"]["EXP"]["level"]}}
+                                        </div>
+                                    </div>
+                                    <div class="expert_detail">
+                                        <div>コンボ {{item["data"]["EXP"]["maxcombo"]}}</div>
+                                        <div>定数　 {{item["data"]["EXP"]["const"]}}</div>
+                                    </div>
+                                </div>
+                                <div class="master">
+                                    <div class="master_container">
+                                        <div class="music_high_level">
+                                            LEVEL
+                                        </div>
+                                        <div class="music_high_level_val">
+                                            {{item["data"]["MAS"]["level"]}}
+                                        </div>
+                                    </div>
+                                    <div class="master_detail">
+                                        <div>コンボ {{item["data"]["MAS"]["maxcombo"]}}</div>
+                                        <div>定数　 {{item["data"]["MAS"]["const"]}}</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="music_bottom">
                         <div class="music_title">{{item["meta"]["title"]}}</div>
-                        <div style="height:inherit;"></div>
+                        <div style=""></div>
                         <div class="divider"></div>
                         <div class="music_artist">{{item["meta"]["artist"]}}</div>
                         <div class="music_bottom_container">
--- viewer/server/api/all.ts
+++ viewer/server/api/all.ts
@@ -1,5 +1,3 @@
-import fs from "fs";
-
 export default async function (req, res) {
     const data = await fetch("https://chunithm.sega.jp/storage/json/music.json")
     const text = await data.text()
